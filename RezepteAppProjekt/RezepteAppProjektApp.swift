c9091145918ef13cbf37d50373f3d90d33c58fbe
Anke Reinelt <ankereinelt@gmx.de>
2021-02-01 23:17:36 +0100

    Initial Commit

---
+++ RezepteAppProjekt/RezepteAppProjektApp.swift
@@ -0,0 +1,17 @@
+//
+//  RezepteAppProjektApp.swift
+//  RezepteAppProjekt
+//
+//  Created by Student on 01.02.21.
+//
+
+import SwiftUI
+
+@main
+struct RezepteAppProjektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
